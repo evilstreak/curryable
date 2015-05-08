20e346a66f13da2f0c0483779a21a85d186f1745
Dominic Baggott <dominic.baggott@gmail.com>
2015-05-08 12:47:54 +0100

    Curry required keyword arguments
    Sidenote: Use a global for the spy rather than injecting it to stop the
    spy being a collaborator in the implementation. Using a global rather
    than a constant prevents having to explicitly unset and reset the
    constant before each run.
    
    + @bestie

--- lib/curryable.rb
+++ lib/curryable.rb
@@ -1,12 +1,44 @@
 class Curryable
-  def initialize(command_class)
+  def initialize(command_class, **arguments)
     @command_class = command_class
+    @arguments = arguments
   end
 
-  attr_reader :command_class
-  private     :command_class
+  attr_reader :command_class, :arguments
+  private     :command_class, :arguments
 
-  def call(**arguments)
+  def call(**new_arguments)
+    combined_arguments = arguments.merge(new_arguments)
+    curryable = self.class.new(command_class, combined_arguments)
+
+    if curryable.enough_arguments?
+      curryable.execute
+    else
+      curryable
+    end
+  end
+
+  protected
+
+  def enough_arguments?
+    provided_keywords & required_keywords == required_keywords
+  end
+
+  def execute
     command_class.new(arguments).call
+  end
+
+  private
+
+  def provided_keywords
+    arguments.keys
+  end
+
+  def required_keywords
+    parameters.map { |(_type, name)| name }
+  end
+
+  def parameters
+    command_class.instance_method(:initialize).parameters
   end
 end
--- spec/curryable_spec.rb
+++ spec/curryable_spec.rb
@@ -3,30 +3,43 @@ require "curryable"
 
 RSpec.describe Curryable do
   class CommandClass
-    def initialize(a:, b:, spy:)
+    def initialize(a:, b:, c:)
       @a = a
       @b = b
-      @spy = spy
+      @c = c
     end
 
     def call
-      @spy.call(a: @a, b: @b)
+      $command_spy.call(a: @a, b: @b, c: @c)
     end
   end
 
   let(:a) { double(:a) }
   let(:b) { double(:b) }
+  let(:c) { double(:c) }
   let(:command_spy) { spy }
 
   subject(:curryable) {
     Curryable.new(CommandClass)
   }
 
+  before do
+    $command_spy = command_spy
+  end
+
   context "when all arguments are provided in one call" do
     it "executes the #call method of the command class" do
-      curryable.call(a: a, b: b, spy: command_spy)
+      curryable.call(a: a, b: b, c: c)
 
-      expect(command_spy).to have_received(:call).with(a: a, b: b)
+      expect(command_spy).to have_received(:call).with(a: a, b: b, c: c)
+    end
+  end
+
+  context "when some arguments are provided" do
+    it "does not execute the #call method of the command class" do
+      curryable.call(a: a)
+
+      expect(command_spy).not_to have_received(:call)
     end
   end
 end
