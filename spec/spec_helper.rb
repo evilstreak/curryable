80d62d5ed7a16bb1f5dcd0fbe108445bbb51995c
Dominic Baggott <dominic.baggott@gmail.com>
2015-05-08 11:43:45 +0100

    Add rspec and pry
    Configure rspec to disable monkey patching and to surface warnings so
    that we can be a well behaved library.
    
    + @bestie

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require "pry"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+    mocks.syntax = :expect
+  end
+
+  config.disable_monkey_patching!
+
+  config.warnings = true
+end
