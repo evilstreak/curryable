282d831241446d1b0102d813d5a590d2c56aa831
Dominic Baggott <dominic.baggott@gmail.com>
2015-08-30 12:59:23 +0100

    Make inspection work for positional only currying

--- spec/required_positional_arguments_spec.rb
+++ spec/required_positional_arguments_spec.rb
@@ -2,15 +2,17 @@ require "spec_helper"
 require "curryable"
 
 RSpec.describe "Required positional arguments" do
-  class RequiredPositionalCommandClass
-    def initialize(a, b, c)
-      @a = a
-      @b = b
-      @c = c
-    end
+  module Curryable::TestClasses
+    class RequiredPositionalCommandClass
+      def initialize(a, b, c)
+        @a = a
+        @b = b
+        @c = c
+      end
 
-    def call
-      $command_spy.call(a: @a, b: @b, c: @c)
+      def call
+        $command_spy.call(a: @a, b: @b, c: @c)
+      end
     end
   end
 
@@ -20,7 +22,7 @@ RSpec.describe "Required positional arguments" do
   let(:command_spy) { spy }
 
   subject(:curryable) {
-    Curryable.new(RequiredPositionalCommandClass)
+    Curryable.new(Curryable::TestClasses::RequiredPositionalCommandClass)
   }
 
   let(:return_value) { double(:return_value) }
@@ -29,6 +31,14 @@ RSpec.describe "Required positional arguments" do
     $command_spy = command_spy
 
     allow(command_spy).to receive(:call).and_return(return_value)
+  end
+
+  context "when no arguments are provided" do
+    it "can be inspected to show all arguments that must be provided" do
+      expect(curryable.inspect).to match(
+        %r{#<Curryable<Curryable::TestClasses::RequiredPositionalCommandClass>:0x[0-9a-f]{12} a=, b=, c=>}
+      )
+    end
   end
 
   context "when all arguments are provided in one call" do
@@ -46,16 +56,24 @@ RSpec.describe "Required positional arguments" do
   end
 
   context "when some arguments are provided" do
-    it "does not execute the #call method of the command class" do
+    subject(:partially_curried) {
       curryable.call(a)
+    }
+
+    it "does not execute the #call method of the command class" do
+      partially_curried
 
       expect(command_spy).not_to have_received(:call)
     end
 
     it "returns a Curryable" do
-      expect(
-        curryable.call(a)
-      ).to be_a(Curryable)
+      expect(partially_curried).to be_a(Curryable)
+    end
+
+    it "can be inspected to show which arguments have been provided and their values" do
+      expect(partially_curried.inspect).to match(
+        %r{#<Curryable<Curryable::TestClasses::RequiredPositionalCommandClass>:0x[0-9a-f]{12} a=#<RSpec::Mocks::Double:0x[0-9a-f]{12} @name=:a>, b=, c=>}
+      )
     end
   end
 
