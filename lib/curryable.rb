bb0147dbc239792655ff8b32300c93c21328425e
Stephen Best <bestie@gmail.com>
2015-08-31 11:15:05 +0100

    Remove unused methods from `Curryable` class

--- lib/curryable.rb
+++ lib/curryable.rb
@@ -2,7 +2,6 @@ require "curryable/parameter_list"
 require "curryable/argument_list"
 
 class Curryable
-  # TODO we could pass an ArgumentList instead of a splat here
   def initialize(command_class, arguments = nil)
     @command_class = command_class
     @arguments = arguments || default_argument_list
@@ -43,22 +42,6 @@ class Curryable
   end
 
   private
-
-  def positional_parameter_names
-    positional_parameters.map(&:name)
-  end
-
-  def positional_parameters
-    parameters.positional
-  end
-
-  def required_keywords
-    parameters.required_keywords.map(&:name)
-  end
-
-  def arity
-    parameters.arity
-  end
 
   def parameters
     ParameterList.new(command_class.instance_method(:initialize).parameters)
